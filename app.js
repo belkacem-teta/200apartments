79056b8bd91a1df48f9b604aecdbccfb1fdc0ef1
belkacem-office <belkacem.teta.work@gmail.com>
2024-10-13 15:49:17 +0100

    fixed landing menu

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ function toggleMenu() {
 
   menu.style.display = (menu.style.display === "none") ? "block" : "none";
 }
+
+document.addEventListener(
+    "click", 
+    function(event) {
+      let menu = document.getElementById("menu");
+      let button = document.getElementById("menu-btn");
+
+      if (menu.style.display === "block" && !menu.contains(event.target) && !button.contains(event.target)) {
+        menu.style.display = "none";
+      }
+    }
+);
